672b60ec6edb48421c0b51995b834d6a011dc9e0
deyaa <deyaa383@gmail.com>
2022-10-01 14:26:12 +0300

    Screenshots

--- Selenium/src/test/java/ParameterJenkinsTest.java
+++ Selenium/src/test/java/ParameterJenkinsTest.java
@@ -1,9 +1,13 @@
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -29,7 +33,18 @@ public class ParameterJenkinsTest {
 		waitElement(By.name("password"), 20, driver.getDriver()).sendKeys(pw);
 		waitElement(By.name("commit"), 20, driver.getDriver()).click();
 
-		Thread.sleep(5000);
+		TakesScreenshot scrShot = ((TakesScreenshot) driver.getDriver());
+
+		// Call getScreenshotAs method to create image file
+
+		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+		// Move image file to new destination
+
+		File DestFile = new File("img.png");
+		FileUtils.copyFile(SrcFile, DestFile);
+
+		// Copy file at destination
 
 	}
 
